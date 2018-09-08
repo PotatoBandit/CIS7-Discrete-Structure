290b46246d9c952dce2d6525dc5622562b71585e
Rebecca <205rcarter@gmail.com>
2018-09-07 17:38:01 -0700

    valid_wff: added feature to input parentheses

---
+++ HW2/main.cpp
@@ -0,0 +1,69 @@
+//Rebecca Carter - ID:2648939
+
+#include<iostream>
+#include<string>
+#include<regex>
+using namespace std;
+
+int parentheses_are_valid(string input);
+
+int main()
+{
+    string input;
+    cout << "Enter in a WFF, and I will tell you if it is valid." << endl;
+    cout << "Type quit to exit" << endl;
+    cout << "Valid Connectors: ^ V ! -> " << endl;
+    cout << "Enter input: ";
+
+    regex reg("^\\s*!?\\(?*\\s*[A-UW-Z]\\s*\\)?*\\s*((\\^|V|->)\\s*!?\\(?*\\s*\\s*[A-UW-Z]\\s*\\)?*\\s*)*$");
+
+    do
+    {
+        getline(cin,input);
+
+        int parentheses = parentheses_are_valid(input);
+
+        if (parentheses != 0)
+        {
+            cout << "invalid parenthetical syntax used." << endl;
+        }
+        else
+        {
+
+            if (regex_match(input,reg))
+            {
+                cout << input << " match." << endl;
+            }
+
+            else if (input != "quit")
+            {
+                cout << input << " No match" << endl;
+            }
+        }
+
+
+    }while(input != "quit");
+    return 0;
+}
+
+ int parentheses_are_valid(string input)
+{
+    int parentheses = 0;
+    for ( int i = 0; i < input.size() ; i ++)
+    {
+        if (input.at(i) == '(')
+        {
+            parentheses ++;
+        }
+        if (input.at(i) == ')')
+        {
+            parentheses--;
+        }
+        if (parentheses < 0)
+        {
+            return parentheses;
+        }
+    }
+    return parentheses;
+}
+
