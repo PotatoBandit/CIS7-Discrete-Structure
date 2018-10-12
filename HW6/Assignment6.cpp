f08652bc48370e5d627f67eb81943f757e761e32
Rebecca C <205rcarter@gmail.com>
2018-10-11 17:01:17 -0700

    added function duration

--- HW6/Assignment6.cpp
+++ HW6/Assignment6.cpp
@@ -1,9 +1,12 @@
+
 #include <iostream>
 #include <regex>
+#include <chrono>
 using namespace std;
 
 void getValues();
 int GCD(int val1, int val2);
+static int stuff = 0;
 
 int main()
 {
@@ -12,6 +15,7 @@ int main()
     int num1 = 0;
     int num2 = 0;
     char ans;
+
     //regular expression for any whole number greater than zero.
     regex rx("[1-9][0-9]*");
 
@@ -32,10 +36,19 @@ int main()
         cout << "Value 2: ";
         cin >> val2;
 
-        } while (!regex_match(val1,rx));
+        } while (!regex_match(val2,rx));
+
+        chrono::high_resolution_clock::time_point t1 = chrono::high_resolution_clock::now();
 
         int gcd = GCD(num1 =stoi(val1), num2 = stoi(val2));
+
+        chrono::high_resolution_clock::time_point t2 = chrono::high_resolution_clock::now();
+
+        auto duration = chrono::duration_cast<chrono::microseconds>( t2 - t1 ).count();
+        auto nanduration = chrono::duration_cast<chrono::nanoseconds>( t2 - t1 ).count();
+
         cout << "GCD of " << val1 << " and " << val2 << " is " << gcd << "." << endl;
+        cout << "Duration: " << duration << " microseconds. (" << nanduration << " nanoseconds)" << endl;
         cout << "Enter another set of values? (Y/N) " << endl;
         cin >> ans;
 
@@ -46,11 +59,10 @@ int main()
 
 int GCD(int num1, int num2)
 {
+        stuff++;
         if (num1 == 0)
         {
             return num2;
         }
         return GCD(num2 % num1, num1);
-
 }
-
